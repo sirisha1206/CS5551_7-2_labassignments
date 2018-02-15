e41b180d9482fcc593ab590d5552c39d8f50acc3
sirisha1206 <nagasirisha1206@gmail.com>
2018-02-14 23:11:48 -0600

    lab2

---
+++ Lab2/Source/app.js
@@ -0,0 +1,4 @@
+function onLoadFunction(){
+    gapi.client.setApiKey('AIzaSyB8yUNoSEk3veNVjETnJNgvw5m3Xqs8nKg');
+    gapi.client.load('plus','v1',function(){})
+}
---
+++ Lab2/Source/script.js
@@ -0,0 +1,33 @@
+var myApp = angular.module("myApp",[]);
+myApp.controller('myController',['$scope',function($scope){
+    // noinspection JSAnnotator
+    $scope.gmail = {
+        username : "",
+        email:""
+    };
+    $scope.onGoogleLogin = function(){
+        var params = {
+            'clientid':'323689101436-a6qrrf9df8amcnh5g05sicek534f329u.apps.googleusercontent.com',
+            'cookiepolicy':'single_host_origin',
+            'callback':function(result){
+                if(result['status']['signed_in']){
+                     var request = gapi.client.plus.people.get(
+                                    {
+                                        'userId':'me'
+                                     }
+                             );
+                    request.execute(function(resp){
+                        $scope.$apply(function(){
+                            $scope.gmail.username = resp.displayName;
+                            $scope.gmail.email = resp.emails[0].value;
+                        })
+                    })
+                }
+            },
+            'approvalprompt':'force',
+            'scope':'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+        };
+        gapi.auth.signIn(params);
+        //$window.location.href="test.html";
+    }
+}]);
